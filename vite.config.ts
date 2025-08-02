78f5711dedd73fae65ed6e471920a665cf5030cc
Andrew <tonkachiov@gmail.com>
2025-08-02 17:25:53 +0300

    four

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://AndrewTnk.github.io/2-3-2',
   test: {
     globals: true,
     environment: 'jsdom',
